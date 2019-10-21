2b49fa6ab559dd03b3afdc597f46c644708f5795
Mabestron <ivangamaliel555@hotmail.com>
2019-10-21 15:41:22 -0500

    Exeptions

---
+++ src/com/Exception_Handling/app/MyExceptions.java
@@ -0,0 +1,28 @@
+package com.Exception_Handling.app;
+
+public class MyExceptions 
+{
+
+	public static void main(String[] args) 
+	{
+		int [] numbers = {1,2,3,4,5,6,7,8,9,10};
+		
+		System.out.println("Element 1 is: "+numbers[0]);
+		System.out.println("Element 2 is: "+numbers[1]);
+		System.out.println("Element 3 is: "+numbers[2]);
+		System.out.println("Element 4 is: "+numbers[3]);
+		System.out.println("Element 5 is: "+numbers[4]);
+		System.out.println("Element 6 is: "+numbers[5]);
+		System.out.println("Element 7 is: "+numbers[6]);
+		System.out.println("Element 8 is: "+numbers[7]);
+		System.out.println("Element 9 is: "+numbers[8]);
+		System.out.println("Element 10 is: "+numbers[9]);
+		System.out.println("Element 11 is: "+numbers[10]);
+		
+		System.out.println("Element 1 is: "+numbers[0]);
+		System.out.println("Element 2 is: "+numbers[1]);
+		System.out.println("Element 3 is: "+numbers[2]);
+
+	}
+
+}
